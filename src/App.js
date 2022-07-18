89b5344d450ed24087e17deb51e87a62b821fbd2
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-07-18 20:33:37 +0530

    mealsItem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import AvailableMeals from "./components/Meals/AvailableMeals";
 import MealsSummary from "./components/Meals/MealsSummary";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <React.Fragment>
       <Header />
       <MealsSummary />
+      <AvailableMeals />
     </React.Fragment>
   );
 }
